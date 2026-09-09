4d73bf6a9f96afc7e7fb49c9895d4040c7a3a43b
Tuborrr-Dev <Israeltubo@gmail.com>
2026-09-09 21:10:06 +0100

    Refactor models to enhance field definitions and relationships for User, Link, and Click classes

--- models/models.py
+++ models/models.py
@@ -1,44 +1,42 @@
 from URL_Shortener.database import Base, engine
 from sqlalchemy import Table, text, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, DeclarativeBase
 
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(100), nullable=False)
-    email = Column(String, unique=True, nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
     created_at = Column(DateTime, server_default=text("now()"))
+    links = relationship("Link", back_populates="owner")
 
 
 class Link(Base):
     __tablename__ = "links"
     id = Column(Integer, primary_key=True)
-    short_code = Column(
-        String(10),
-        unique=True,
-        nullable=False,
-    )
-    long_url = Column(String(200), nullable=False)
+    short_code = Column(String(10), unique=True, nullable=False, index=True)
+    long_url = Column(String(2024), nullable=False)
     created_at = Column(DateTime, server_default=text("now()"))
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="SET NULL"), nullable=True
+    )  # <-- allowing unregistered users to have links left when they are unregistered
+    owner = relationship("User", back_populates="links")
+    clicks = relationship("Click", back_populates="link", cascade="all, delete-orphan")
 
 
 class Click(Base):
     __tablename__ = "clicks"
     id = Column(Integer, primary_key=True)
-    link_id = Column(Integer, ForeignKey("links.id"))
+    # we have to delete clicks for a link if deleted
+    link_id = Column(
+        Integer, ForeignKey("links.id"), ondelete="CASCADE", nullable=False
+    )
     clicked_at = Column(DateTime, server_default=text("now()"))
     ip_hash = Column(String(100))
-    referrer = Column(String(100))
+    referrer = Column(String(250))
+    link = relationship("Link", back_populates="clicks")
 
-
-# we have to delete clicks for a link if deleted
-click_links = Table(
-    "links_clicks",
-    Base.metadata,
-    Column("link_id", ForeignKey("links.id", ondelete="CASCADE"), primary_key=True),
-    Column("click_id", ForeignKey("clicks.id", ondelete="CASCADE"), primary_key=True),
-)
 
 # create at all instances
 Base.metadata.create_all(engine)
