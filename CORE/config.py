f544b6525bcfee7da146dfe4287f59ae3a88265a
Tuborrr-Dev <Israeltubo@gmail.com>
2026-09-12 14:59:41 +0100

    Add REDIS_URL to Settings class for configuration

--- CORE/config.py
+++ CORE/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 # this is written to get the files ws have in the .env file
 class Settings(BaseSettings):
     DATABASE_URL: str
+    REDIS_URL: str
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
