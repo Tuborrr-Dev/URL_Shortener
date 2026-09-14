f7f382382796f2d040ddb5b6346ed2ef10288347
Tuborrr-Dev <Israeltubo@gmail.com>
2026-09-14 12:22:21 +0100

    Refactor count_em_24hr to include cache check and calculate clicks in the past 24 hours

--- crud/url_stats.py
+++ crud/url_stats.py
@@ -2,6 +2,10 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+# this is calculating the date
+import time
+from datetime import datetime, timezone
+
 # for accessing DB
 from URL_Shortener.models.models import Link, Click
 
@@ -38,10 +42,16 @@ def count_em_24hr(short_code: str, db: Session) -> int:
     # for the past 24 hours we do a filter based off timestamp
     # we first checked if it exists which it does so it's definitely in cache
     cached_data = r.get(short_code)
+    if not cached_data:
+        return None
     data = json.loads(cached_data)
     link_id = data["id"]
-    # now we have link ID so we count em up
-    total_clicks = (
-        db.query(func.count(Click.id)).filter(Click.link_id == link_id).scalar()
-    )  # <-- the func count is the sqlalchemy equivalent of count in TEXT
-    return total_clicks
+    # now we have link ID so we calculate using epoch to know exact 24 hour ago
+    limit_epoch = int(time.time()) - 86400  # 86400 is 24hrs ago in secs
+    total_clicks_24hr = (
+        db.query(func.count(Click.id))
+        .filter(Click.link_id == link_id)
+        .filter(Click.clicked_at >= limit_epoch)
+        .scalar()
+    )
+    return total_clicks_24hr
