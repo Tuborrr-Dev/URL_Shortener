d354fde0165eef126096f340f156a11a35f13405
Tuborrr-Dev <Israeltubo@gmail.com>
2026-09-17 19:17:44 +0100

    Fix import statement for routes in main.py to maintain organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from URL_Shortener.api.routes import (
-    router,
+from URL_Shortener.api import (
+    routes,
 )  # this isnt done in the main.py file because we want to keep the main.py file clean and organized
 
 app = FastAPI(title="URL Shortener API", version="1.0.0")
