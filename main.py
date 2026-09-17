f5aee54228ffdad461b10561a1033746027da07c
Tuborrr-Dev <Israeltubo@gmail.com>
2026-09-17 19:29:09 +0100

    Fix import statement for router in main.py to improve organization

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from URL_Shortener.api import (
-    routes,
+from URL_Shortener.api.routes import (
+    router,
 )  # this isnt done in the main.py file because we want to keep the main.py file clean and organized
 
 app = FastAPI(title="URL Shortener API", version="1.0.0")
 
-# Mount your endpoints to the main application
-app.include_router(url_router)
+app.include_router(router)
 
 
 @app.get("/health")
